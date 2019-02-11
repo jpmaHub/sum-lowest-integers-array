f3084aaa35545b8c3ded879b54d3e0d6be2199e2
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2019-02-11 07:54:56 +0000

    Refactor solution

--- SumApp/SumApp/Kata.cs
+++ SumApp/SumApp/Kata.cs
@@ -7,19 +7,15 @@ namespace SumApp
     {
         public static int sumTwoSmallestNumbers(int[] numbers)
         {
-            
-            if (numbers.Length >= 2)
+            try
             {
                 Array.Sort(numbers);
                 return numbers[0] + numbers[1];
-                /*
-                var a = numbers[0] < numbers[1] ? numbers[0] : numbers[1];
-                var b = numbers[1] < numbers[2] ? numbers[1] : numbers[2];
-                return a + b; */
             }
-            
-            return numbers.Length == 0 ? 0 : numbers[0] + numbers[1];
-           
+            catch (IndexOutOfRangeException)
+            {
+                return 0;
+            } 
         } 
     }
 }
